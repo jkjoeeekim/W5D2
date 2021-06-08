60b503f581e10e0a61884402b3a361e4685d26ba
nichathong <nicha.fn@gmail.com>
2021-06-08 16:37:37 -0700

    completed #highest_gdp

--- sql_project/skeleton/lib/04a_countries_bonus.rb
+++ sql_project/skeleton/lib/04a_countries_bonus.rb
@@ -14,6 +14,22 @@ def highest_gdp
   # Which countries have a GDP greater than every country in Europe? (Give the
   # name only. Some countries may have NULL gdp values)
   execute(<<-SQL)
+  SELECT
+    name
+  FROM
+    countries
+  WHERE
+    gdp > (
+      SELECT
+        gdp
+      FROM
+        countries
+      WHERE
+        continent = 'Europe' AND gdp IS NOT NULL
+      ORDER BY
+        gdp DESC
+        LIMIT 1
+    );
   SQL
 end
 
